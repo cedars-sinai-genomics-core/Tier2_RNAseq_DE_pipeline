dba5f4948974fa4098ccd24aa7345a73ec04c68f
Alex Rajewski <araje002@ucr.edu>
2021-05-17 18:06:40 -0700

    temporary path

--- RNAseq_tier2/RNAseq_analysis.sh
+++ RNAseq_tier2/RNAseq_analysis.sh
@@ -29,4 +29,4 @@ fi
 
 # get PCA plots for all samples and DEGs table, interactive report for each comparison
 #/hpc/apps/R/3.4.1/bin/Rscript /common/genomics-core/data/Temp/Di_RNA_seq_test/downstream_test/RNAseq_tier2.R $1 $2 $3 $4
-/usr/bin/Rscript /home/genomics/genomics/apps/RNAseq_tier2/RNAseq_tier2.R $1 $2 $3 $4
+/usr/bin/Rscript /home/genomics/genomics/apps/Tier2_RNAseq_DE_pipeline/RNAseq_tier2/RNAseq_tier2.R $1 $2 $3 $4
